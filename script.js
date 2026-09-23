510b2e705dd57d4aef364c9e56cd0c2dd36037e2
leonardoborges-hash <leonardo.borges@escola.pr.gov.br>
2026-09-23 14:22:34 -0300

    GG

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const botoesCurtir = document.querySelectorAll(".curtir");
-boesCurtir.forEach(function(botoaoCurtir){
+
+botoesCurtir.forEach(function(botaoCurtir) {
+
     let curtiu = false;
-    botaoCurtiu.addEventListener("click , curtir");
-function curtir() {
-    const contador = botoaCurtir.queryselector("span");
-    if(curtiu === false){
-        contador.textContent++;
-    curtiu = true;}
-    else{
-        contador.texContent--;
-        curtiu = false;
+
+    botaoCurtir.addEventListener("click", curtir);
+
+    function curtir() {
+
+        const contador = botaoCurtir.querySelector("span");
+
+        if (curtiu === false) {
+            contador.textContent++;
+            curtiu = true;
+        } else {
+            contador.textContent--;
+            curtiu = false;
+        }
     }
-}
-}
+});
